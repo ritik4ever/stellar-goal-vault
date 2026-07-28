02fa1ab7cf2d2dc4608288b916b620d1596914d3
Silver36-ship-it <emycallistus@gmail.com>
2026-07-28 23:20:56 +0100

    feat(frontend): Add Creator dashboard page and route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import { NotFoundPage } from "./components/NotFoundPage";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import CreatorDashboard from './pages/CreatorDashboard';
+import { NotFoundPage } from './components/NotFoundPage';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/campaigns/:id" element={<App />} />
+        <Route path="/dashboard" element={<CreatorDashboard />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
